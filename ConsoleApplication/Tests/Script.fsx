8e929b8460d097e502614eca21966b7ec28dbd05
zstone1 <zstonex@gmail.com>
2017-06-17 10:20:44 -0700

    adding tests

---
+++ ConsoleApplication/Tests/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Tests.fs"
+open Tests
+
+// Define your library scripting code here
+
---
+++ ConsoleApplication/Tests/Tests.fs
@@ -0,0 +1,31 @@
+﻿module ParserTests
+open FParsec
+open NUnit.Framework
+open NUnit.Framework.Constraints
+open Parser
+
+[<Test>]
+let ``one plus one`` ()= Assert.IsTrue(1+1=2)
+  
+let parseComparison parser str expected = 
+  match run parser str with
+  | Success(result,_,_) -> Assert.AreEqual(expected, result)
+  | x -> failwithf "failed to parse %s with %A" str x
+  
+let exprCompare = parseComparison parseExpression
+
+[<Test>]
+let ``parse int literal``() = 
+  (123 |> IntLit |>Atom) |> exprCompare "123 " 
+
+let strLitCompare input result = (result |> StringLit |> Atom) |> exprCompare input
+[<Test>]
+let ``parse string literals``() = "foo" |> strLitCompare "\"foo\"" 
+[<Test>]
+let ``escape slashes``() = @"a\" |> strLitCompare @"""a\\"""
+[<Test>]
+let ``escape others``() = "\n\r" |> strLitCompare @"""\n\r"""
+
+[<Test>]
+let ``empty function`` = Func (( 
+
