45007e0e839406f32c8df9020f84a4a490267971
zstone1 <zstonex@gmail.com>
2017-06-16 21:13:24 -0700

    initial commit. Writing parser

---
+++ ConsoleApplication/Library/Library.fs
@@ -0,0 +1,18 @@
+﻿module Parser
+open FParsec
+open FParsec.CharParsers
+
+type Literal = 
+ | IntLit of int
+ | StringLit of string
+
+type Expression =
+ | Atom of Literal
+ | Func of string * (Expression list)
+ 
+
+let ParseInt = pint32 .>> spaces |>> IntLit
+ 
+ 
+let Parse = run ParseInt
+
---
+++ ConsoleApplication/Runner/Program.fs
@@ -0,0 +1,8 @@
+﻿module runner
+
+let someFSharpFunction (x:string) = x
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
