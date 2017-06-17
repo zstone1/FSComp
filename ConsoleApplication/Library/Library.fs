9fc232ff27c4cef6db8dc3d031348a537001ef57
zstone1 <zstonex@gmail.com>
2017-06-16 23:49:50 -0700

    adding ignore file

--- ConsoleApplication/Library/Library.fs
+++ ConsoleApplication/Library/Library.fs
@@ -2,6 +2,8 @@
 open FParsec
 open FParsec.CharParsers
 
+exception CompilerError of string
+
 type Literal = 
  | IntLit of int
  | StringLit of string
@@ -16,6 +18,19 @@ type Statement =
  | Execution of Expression
  | Declaration of string * string
  | Assignment of string * Expression
+ 
+type FuncSignature = {
+  access : string
+  isStatic : bool
+  returnTy : string
+  name : string
+  args : (string * string) list
+}
+
+type Function = {
+  signature : FuncSignature
+  body : Statement list
+}
  
 type ParserData = unit
  
@@ -39,8 +54,8 @@ let rec parseExpression' (): Parser<Expression,ParserData> =
   let parseFunc = parse {
       let! name = parseName 
       do! (tok "(") 
-      do! (tok ")") 
       let! args = sepBy (parseExpression' ()) (tok ",")
+      do! (tok ")") 
       return Func (name, args)
   }
   parseAtom <|> parseFunc <?> "Failed to parse expression"
@@ -48,19 +63,12 @@ let rec parseExpression' (): Parser<Expression,ParserData> =
 let parseExpression = parseExpression'()
 
 let rec parseStatement' () : Parser<Statement, ParserData> = 
-  let parseBody =  between (tok "{") (tok "}") (sepBy (parseStatement'()) (tok ";"))
   let parseReturn = tok "return " >>. parseExpression |>> ReturnStat
-  let parseIfStat = parse {
-    let! guard = between (tok "(") (tok ")") (parseExpression)
-    let! body = parseBody
-    return IfStat (guard, body)
-  }
+  let parseIfStat = between (tok "(") (tok ")") (parseExpression) 
+                    .>>. parseBody
+                    |>> IfStat 
   let parseExecution = parseExpression |>> Execution
-  let parseDeclaration = parse {
-    let! ty = parseName
-    let! name = parseName
-    return Declaration (ty,name) 
-  }
+  let parseDeclaration = parseName .>>. parseName |>> Declaration 
   let parseAssignment = parse{
     let! name = parseName
     do! tok "="
@@ -73,9 +81,40 @@ let rec parseStatement' () : Parser<Statement, ParserData> =
   parseExecution <|> 
   parseDeclaration <|>
   parseAssignment
+and parseBody =  between (tok "{") (tok "}") (sepBy (parseStatement'()) (tok ";"))
 
+let parseStatement = parseStatement'()
 
+let parseSignature : Parser<FuncSignature, ParserData> = 
+  let parseSignature isStatic = parse{
+    let! access = parseName
+    do! if isStatic then tok "static" else preturn ()
+    let! returnTy = parseName
+    let! name = parseName
+    let! args = sepBy (parseName .>>. parseName) (tok ",")
+    return {
+      access = access
+      isStatic = isStatic
+      returnTy = returnTy
+      args = args
+      name = name
+    }
+  }
+  parseSignature true <|> parseSignature false
+  
+let parseFunction : Parser<Function, ParserData> = parse {
+  let! signature = parseSignature
+  let! body = parseBody
+  return {signature = signature; body = body}
+}
 
+let parseModule = many parseFunction
+
+let public parseProgram s : Function list =
+  match run parseModule s with
+  | Success(result,_,_) -> result
+  | Failure(err,_,_) -> raise (CompilerError err)
+  
 
 
 
