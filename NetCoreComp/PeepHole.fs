165baacd82455dcb5f2e446ad1df52d2b43456a5
zstone1 <zstonex@gmail.com>
2017-09-25 10:36:03 -0700

    forgot files

---
+++ NetCoreComp/PeepHole.fs
@@ -0,0 +1,26 @@
+module PeepHole
+open ASTBuilder
+open FSharp.Collections;
+open FSharpx
+open Flatten
+open ComputationGraph
+open AssignHomes
+open FSharpx.State
+open Option
+open MixedLang
+open Unification
+open InjectMoves
+
+let cullNoOps = function 
+  | MovA (p,q) when p = q -> None
+  | SubA (_, Imm 0) | AddA (_, Imm 0) -> None
+  | Assembly.Nop -> None
+  | x -> Some x
+
+let peepHoleOptimizeFunc asm = List.choose cullNoOps asm
+  
+
+let peepHoleOptimize (m : AsmModule) = {
+    lits = m.lits
+    funcs = m.funcs |> List.map peepHoleOptimizeFunc
+}
---
+++ NetCoreComp/PruneDeadCode.fs
@@ -0,0 +1,34 @@
+module PruneDeadCode
+open Flatten
+open ASTBuilder
+open FSharpx.State
+open FSharpx
+open Aether
+open ComputationGraph
+open MixedLang
+
+
+let private pruneUnusedAssignments ml =
+  //Note that it's critical that everything here retains order.
+  let graph = ml |> toGraph
+  let traverals = graph |> getTraversals
+  let usedIds = traverals |> Seq.collect (fun (_,i) -> i.usedAssignments)
+  let isUsed nodeId = function  
+    | {instruction = AssignI (MLVarName _, _)} -> usedIds |> Seq.contains nodeId 
+    | _ -> true
+  let pruned = graph.nodes 
+            |> Map.filter isUsed 
+            |> Map.map (fun k v -> v.instruction)
+  pruned |> mapValues
+
+let rec private pruneMlUntilDone' lastOne ml = 
+  if lastOne = List.length ml
+  then ml 
+  else pruneMlUntilDone' (List.length ml) (pruneUnusedAssignments ml)
+
+let private pruneMlUntilDone x = pruneMlUntilDone' -1 x
+
+let pruneDeadBranches (m:MLModule) = {
+    lits = m.lits
+    funcs = m.funcs |>List.map (snd_set pruneMlUntilDone)
+}
